e8be30d7259390176ee1306872a0c4c052eb193b
Sabrina <sketelone@gmail.com>
2023-02-16 15:33:35 -0800

    create linked list module and test suite

---
+++ LinkedList.js
@@ -0,0 +1,160 @@
+
+const LinkedList = () => {
+
+    let NodeList = {};
+
+    const append = (value) => {
+    //function to add new node containing value to the end of the list
+        let node = MyNode(value);
+        // console.log("appending: ",node)
+        if (Object.keys(NodeList).length !== 0) {
+            let lastNode = tail();
+            lastNode.nextNode = node;
+        } else {
+            NodeList = node;
+        }
+        return NodeList;
+    }
+
+    const prepend = (value) => {
+    //function to add new node containing value to the start of the list
+        // console.log("prepending: ",node)
+        let node = MyNode(value);
+        node.nextNode = NodeList;
+        NodeList = node;
+        return NodeList;
+    }
+
+    const size = (obj) => {
+    //returns the total number of nodes in the list by recursively traversing linked list
+    //and counting elements until it hits the end
+        let list = obj || NodeList;
+        return list.nextNode == null ? 1 : 1+size(list.nextNode);
+    }
+
+    const head = () => {
+    //returns the first node in the list
+        return NodeList;
+    }
+
+    const tail = (obj) => {
+    //returns the last node in the list by recursively traversing linked list until
+    //it hits a node with a 'null' next node
+        let list = obj || NodeList;
+        let tailNode = {};
+        if (list.nextNode == null) {
+            // console.log("we're at the tail")
+            tailNode = list;
+            return tailNode;
+        } else {
+            // console.log("go one down")
+            tailNode = tail(list.nextNode)
+        }
+        return tailNode;
+    }
+
+    const at = (index, obj, num) => {
+    //returns the node at the given index
+        let list = obj || NodeList;
+        let n = num || 0;
+        if (index == n) {
+            return list;
+        } else {
+            n++;
+            return at(index, list.nextNode, n)
+        }
+    }
+
+    const pop = (obj) => {
+    //removes the last element from the list
+        let list = obj || NodeList;
+        if(list.nextNode.nextNode == null) {
+            list.nextNode = null;
+            return list;
+        } else {
+            return pop(list.nextNode);
+        }
+    }
+
+    const contains = (value, obj) => {
+    //returns true if passed in value is in the list, else false
+        let list = obj || NodeList;
+        if (list.nextNode == null) {
+            return false;
+        } else if (list.value == value) {
+            return true;
+        } else {
+            return contains(value, list.nextNode);
+        }
+    }
+
+    const find = (value, obj, num) => {
+    //returns the index of the node containing value, or null if not found
+        let list = obj || NodeList;
+        let n = num || 0;
+        if (list.value == value) {
+            return n;
+        } else if (list.nextNode == null){
+            return false;
+        } else {
+            n++;
+            return find(value, list.nextNode, n)
+        }   
+    }
+
+    const insertAt = (value, index, obj, num) => {
+    //inserts node at the given index
+        let node = MyNode(value);
+        let list = obj || NodeList;
+        let n = num || 0 ;
+        if (n+1 == index) {
+            node.nextNode = list.nextNode;
+            list.nextNode = node;
+            return list;
+        } else {
+            n++;
+            return insertAt(value, index, list.nextNode, n)
+        }
+    }
+
+    const removeAt = (index, obj, num) => {
+    //removes node at given index
+        let list = obj || NodeList;
+        let n = num || 0;
+        if (n+1 == index) {
+            var temp = list.nextNode.nextNode;
+            list.nextNode = temp;
+            // list.nextNode = list.nextNode.nextNode;
+            return list;
+        } else {
+            n++;
+            return removeAt(index, list.nextNode, n)
+        }
+
+    }
+
+    const toString = (obj) => {
+    //returns LinkedList as a string
+        // console.log("toString", NodeList)
+        let string = "";
+        let list = obj || NodeList;
+        if (list.nextNode == null) {
+            string = string.concat(`( ${list.value} ) -> null`);
+            return string;
+        } else {
+            string = string.concat(`( ${list.value} ) -> `, toString(list.nextNode))
+        }
+        return string;
+    }
+
+    return {append, prepend, size, head, tail, at, pop, contains, find, insertAt, removeAt, toString};
+};
+
+const MyNode = (val, node) => {
+    const value = val || null;
+    const nextNode = node || null;
+
+    return {value, nextNode};
+}
+
+module.exports = LinkedList;
---
+++ LinkedList.test.js
@@ -0,0 +1,155 @@
+const LinkedList = require("./LinkedList.js")
+
+//initalize linkedList()
+const linkedList = LinkedList();
+
+test('append adds correct number of items', () => {
+    let nodes = ["A", "B", "C", "D", "E"];
+    nodes.forEach(node => {
+        linkedList.append(node);
+    })
+    expect(linkedList.size()).toBe(5)
+})
+
+test('prepend adds correct number of items', () => {
+    let newNodes = [1, 2, 3, 4, 5];
+    newNodes.forEach(node => {
+        linkedList.prepend(node);
+    })
+    expect(linkedList.size()).toBe(10)
+})
+
+test('size returns length a number', () => {
+    expect(typeof linkedList.size()).toBe('number')
+});
+
+test('size returns length of list', () => {
+    expect(linkedList.size()).toBe(10)
+});
+
+test('head returns first element', () => {
+    let list = {value: 5, nextNode: 
+                    {value: 4, nextNode:
+                        {value: 3, nextNode: 
+                            {value: 2, nextNode:
+                                {value: 1, nextNode:
+                                    {value: "A", nextNode:
+                                        {value: "B", nextNode:
+                                            {value: "C", nextNode:
+                                                {value: "D", nextNode:
+                                                    {value: "E", nextNode: null}
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+    expect(linkedList.head()).toStrictEqual(list)
+});
+
+test('tail returns last element', () => {
+    expect(linkedList.tail()).toStrictEqual({value: "E", nextNode: null})
+});
+
+test('toString returns a string', () => {
+    expect(typeof linkedList.toString()).toBe('string')
+})
+
+test('toString returns correct string', () => {
+    expect(linkedList.toString()).toBe('( 5 ) -> ( 4 ) -> ( 3 ) -> ( 2 ) -> ( 1 ) -> ( A ) -> ( B ) -> ( C ) -> ( D ) -> ( E ) -> null')
+})
+
+test('contains returns true if value in list', () => {
+    expect(linkedList.contains(3)).toBeTruthy()
+})
+
+test('contains returns false if value not in list', () => {
+    expect(linkedList.contains("G")).toBeFalsy()
+})
+
+test('at returns correct node', () => {
+    let node = {value: 2, nextNode:
+                    {value: 1, nextNode:
+                        {value: "A", nextNode:
+                            {value: "B", nextNode:
+                                {value: "C", nextNode:
+                                    {value: "D", nextNode:
+                                        {value: "E", nextNode: null}
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+    expect(linkedList.at(3)).toStrictEqual(node);
+})
+
+test('at returns head when index is 0', () => {
+    expect(linkedList.at(0)).toBe(linkedList.head())
+})
+
+test('at returns head when index is at the tail', () => {
+    expect(linkedList.at(9)).toBe(linkedList.tail())
+})
+
+test('pop removes one element', () => {
+    linkedList.pop()
+    expect(linkedList.size()).toBe(9)
+})
+
+test('pop removes last element', () => {
+    linkedList.append("E")
+    linkedList.pop()
+    expect(linkedList.tail()).toStrictEqual({value: "D", nextNode: null})
+})
+
+test('find returns index as a number', () => {
+    expect(typeof linkedList.find("D")).toBe('number');
+})
+
+test('find returns correct index', () => {
+    expect(linkedList.find("D")).toBe(8);
+})
+
+test('find returns false if item not found', () => {
+    expect(linkedList.find("X")).toBeFalsy();
+})
+
+test('insertAt adds one element', () => {
+    linkedList.append("E")
+    linkedList.insertAt("X",5)
+    expect(linkedList.size()).toBe(11);
+})
+
+test('insertAt inserts value', () => {
+    expect(linkedList.contains("X")).toBeTruthy();
+})
+
+test('insertAt inserts value at correct node', () => {
+    expect(linkedList.find("X")).toBe(5);
+})
+
+test('removeAt removes one element', () => {
+    linkedList.removeAt(5)
+    expect(linkedList.size()).toBe(10);
+})
+
+test('removeAt removes correct value', () => {
+    expect(linkedList.contains("X")).toBeFalsy();
+})
+
+test('removeAt removes value at correct node', () => {
+    let node = {value: "A", nextNode:
+                    {value: "B", nextNode:
+                        {value: "C", nextNode:
+                            {value: "D", nextNode:
+                                {value: "E", nextNode: null}
+                            }
+                        }
+                    }
+                }
+    expect(linkedList.at(5)).toStrictEqual(node);
+})
